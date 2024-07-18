ecfc0ce0f1b76e92a3cb19f8bcd835ceecf5281c
bae080311 <156505831+bae080311@users.noreply.github.com>
2024-07-18 23:56:20 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ JavaScript/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,14 +1,4 @@
 function solution(num_list) {
-    let num = 1;
-    let num1 = 0;
-    for (let i=0;i<num_list.length;i++){
-        num *= num_list[i];
-        num1 += num_list[i];
-    }
-    if (num<num1**2){
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    var answer = 0;
+    return answer;
 }
