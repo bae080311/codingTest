4fcabe9358e9aea63944b0ceb66e9f10d5acacdf
bae080311 <156505831+bae080311@users.noreply.github.com>
2024-07-19 10:10:22 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let odd = "";
+    let even = "";
+
+    for (let num of num_list) {
+        if (num % 2 === 0) {
+            even += num.toString();
+        } else {
+            odd += num.toString();
+        }
+    }
+    return Number(odd)+ Number(even);
+}
