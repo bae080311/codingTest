d44aead4d4295833f1298a82814397f0296ac62a
배경진 <156505831+bae080311@users.noreply.github.com>
2024-08-12 08:17:46 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+    num_list.forEach((v, i) => {
+        i % 2 === 0 ? even += v : odd += v;
+    });
+    return even > odd ? even : odd;
+}
