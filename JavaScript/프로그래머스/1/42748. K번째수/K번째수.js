0f8ee897f0979e5c2a0c1562d610f93a9d828a91
배경진 <156505831+bae080311@users.noreply.github.com>
2024-09-03 11:07:32 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    let result = [];
+
+    for (let i = 0; i < commands.length; i++) {
+        result.push(array.slice(commands[i][0] - 1, commands[i][1]).sort((A, B) => A -B)[commands[i][2] - 1]);
+    }
+    return result;
+}
