0d5a33a499d5436f52845b2a1381c5b30866f47d
배경진 <156505831+bae080311@users.noreply.github.com>
2024-11-08 14:34:50 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.13 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var answer = [];
+    for (let i = 0; i < n; i++) {
+        let temp = Array(n).fill(0);
+        temp[i] = 1;  
+        answer.push(temp); 
+    }
+    return answer;
+}
