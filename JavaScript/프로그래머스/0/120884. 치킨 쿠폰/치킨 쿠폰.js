b3f1f6441fa1b499e31e6ab1be2c8be7a22ba979
배경진 <156505831+bae080311@users.noreply.github.com>
2024-11-28 23:15:23 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,9 @@
+function solution(chicken) {
+    var answer = 0;
+    while (chicken >= 10) {
+        let service = Math.floor(chicken / 10);
+        answer += service; 
+        chicken = service + (chicken % 10); 
+    }
+    return answer;
+}
