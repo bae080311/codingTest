a5402467d63ac3ca94b4064ef227052f891c2ff3
배경진 <156505831+bae080311@users.noreply.github.com>
2024-12-10 23:51:08 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,9 @@
+function solution(id_pw, db) {
+    const [inputId, inputPw] = id_pw;
+    for (const [id, pw] of db) {
+        if (id === inputId) {
+            return pw === inputPw ? "login" : "wrong pw";
+        }
+    }
+    return "fail";
+}
