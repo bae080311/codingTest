6a7d26f1c804869979cb4ee3d79c89c1e908e887
배경진 <dev.kyungjin@gmail.com>
2026-03-11 14:41:56 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    let num1 = 1;
+    
+    for (let i = 1; i <= Math.min(a, b); i++) {
+        if (a % i === 0 && b % i === 0) num1 = i;
+    }
+    
+    a /= num1;
+    b /= num1;
+    
+    while (b % 2 === 0) b /= 2;
+    while (b % 5 === 0) b /= 5;
+    
+    return b === 1 ? 1 : 2;
+}
